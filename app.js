88df1c9b5aebb4bc0ba6770f2e9b5dcf98e2c755
吴昊 <wuhao@admaster.com.cn>
2015-06-10 17:50:40 +0800

    第一次提交

---
+++ app.js
@@ -0,0 +1,114 @@
+function index(page){
+    var page = parseInt(page) || 1;
+    window._G = window._G || {post: {}, postList: {}};
+    $('#title').html(_config['blog_name']);
+    if(_G.postList[page] != undefined){
+      $('#container').html(_G.postList[page]);
+      return;
+    }
+
+    $.ajax({
+        url:"https://api.github.com/repos/"+_config['owner']+"/"+_config['repo']+"/issues",
+        data:{
+            filter       : 'created',
+            page         : page,
+            per_page     : _config['per_page'],
+            access_token : _config['access_token']
+        },
+        beforeSend:function(){
+          $('#container').html('<center><img src="loading.gif" class="loading"></center>');
+        },
+        success:function(data, textStatus, jqXHR){
+            var link = jqXHR.getResponseHeader("Link") || "";
+            var next = false;
+            var prev = false;
+            if(link.indexOf('rel="next"') > 0){
+              next = true;
+            }
+            if(link.indexOf('rel="prev"') > 0){
+              prev = true;
+            }
+            var ractive = new Ractive({
+                template : '#listTpl',
+                data     : {
+                    posts : data,
+                    next  : next,
+                    prev  : prev,
+                    page  : page
+                }
+            });
+            window._G.postList[page] = ractive.toHTML();
+            $('#container').html(window._G.postList[page]);
+
+            //将文章列表的信息存到全局变量中，避免重复请求
+            for(i in data){
+              var ractive = new Ractive({
+                  template : '#detailTpl',
+                  data     : {post: data[i]}
+              });
+              window._G.post[data[i].number] = {};
+              window._G.post[data[i].number].body = ractive.toHTML();
+
+              var title = new Ractive({
+                 template: '#titleTpl',
+                 data: {title: data[i].title + " | " + _config['blog_name']}
+              });     
+              window._G.post[data[i].number].title = title.toHTML();
+            }
+        }
+    });
+}
+
+function detail(id){
+    if(!window._G){
+      window._G = {post: {}, postList: {}};
+      window._G.post[id] = {};  
+    }
+    
+    if(_G.post[id].body != undefined){
+      $('#container').html(_G.post[id].body);
+      $('#title').html(_G.post[id].title);
+      return;
+    }
+    $.ajax({
+        url:"https://api.github.com/repos/"+_config['owner']+"/"+_config['repo']+"/issues/" + id,
+        data:{
+            access_token:_config['access_token']
+        },
+        beforeSend:function(){
+          $('#container').html('<center><img src="loading.gif" alt="loading" class="loading"></center>');
+        },
+        success:function(data){
+            var ractive = new Ractive({
+                 template: '#detailTpl',
+                 data: {post: data}
+            });
+            
+            window._G.post[id].body = ractive.toHTML();
+            $('#container').html(window._G.post[id].body);
+
+            var title = new Ractive({
+               template: '#titleTpl',
+               data: {title: data.title + " | " + _config['blog_name']}
+            });     
+            window._G.post[id].title = title.toHTML();
+            $('#title').html(window._G.post[id].title);
+        }
+    });    
+}
+
+var helpers = Ractive.defaults.data;
+helpers.toHTML = function(content){
+    return markdown.toHTML(content,'Maruku');
+}
+helpers.formatTime = function(time){
+    return time.substr(0,10);
+}
+
+var routes = {
+    '/': index,
+    'p:page': index,
+    'post/:postId': detail
+};
+var router = Router(routes);
+router.init('/');
---
+++ config.js
@@ -0,0 +1,7 @@
+var _config = {
+    blog_name : '耗子吴博客',
+    owner: 'lifesinger',
+    repo: 'lifesinger.github.com',
+    per_page: '15',           // 默认一页显示几篇文章
+    access_token: 'acfebc64480c5f52a7d81f202dc83818cfe98c13'
+}
