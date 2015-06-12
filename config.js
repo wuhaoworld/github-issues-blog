2ca9e813b5e069a81cd68792485074f244a78419
吴昊 <wuhao@admaster.com.cn>
2015-06-12 14:02:24 +0800

    修改了一些细节

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
-    blog_name : '用于演示的博客',    // 博客名称
+    blog_name : '用于演示的博客',   // 博客名称
     owner: 'lifesinger',          // github 用户名
     repo: 'lifesinger.github.com',// github 中对应项目名
-    per_page: '15',               // 默认一页显示几篇文章
-    access_token: 'ef66bb01c611799d7b9ad773a91a701785fbf425' //access_token
+    // access_token: '',          // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    per_page: '15'                // 默认一页显示几篇文章
 }
