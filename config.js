2271acb73bb2e4de3afce414c5e02e6c2852788c
betarvel <betarvel@outlook.com>
2015-11-03 15:12:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name       : "Betarvel's whisper",         // 博客名称
     owner           : 'betarvel',           // github 用户名
-    repo            : 'betarvel.github.com',// github 中对应项目名
+    repo            : 'issues-blog',// github 中对应项目名
     duoshuo_id      : 'betarvel',            // 在第三方评论插件多说申请站点的子域名
     // access_token : 'abcde'+'fghijk',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     per_page        : '8'                    // 默认一页显示几篇文章
