6a7bbd44e88f22022474181c9c82fe13783ec5a8
虫子樱桃 <czytcn@gmail.com>
2017-06-02 10:29:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 var _config = {
     blog_name       : '虫子樱桃の博客',         // 博客名称
     owner           : 'czytcn',           // github 用户名
-    repo            : 'czytcn.github.com',// github 中对应项目名
-    duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
+    repo            : 'czytcn.github.com',// github 中对应项目名   duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
      access_token : '9798923ed4a1006befb3e68bc64f81ab0e692b74',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
     per_page        : '15'                    // 默认一页显示几篇文章
 }
