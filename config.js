026bf3575024ddf3249d3237bf9ab361f34f4807
chenhonzhou <m15107415590@163.com>
2017-08-16 16:39:02 +0800

    需要说说

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var _config = {
     blog_name       : '陈宏洲的BLOG',           // 博客名称
-    owner           : 'chenhonzhou',           // github 用户名
-    repo            : 'blog',    // github 中对应项目名
+    owner           : 'atrance',           // github 用户名
+    repo            : 'blog-post',    // github 中对应项目名
 
 	//    duoshuo_id      : 'hello1234',            // 在第三方评论插件多说申请站点的子域名
     // access_token : 'abcde'+'fghijk',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
